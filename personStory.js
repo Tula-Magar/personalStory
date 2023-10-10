04cfc5c0b852a04296eaaf8893efd881394c2c1c
tmagar <tmagar448@gmail.com>
2023-10-10 15:31:33 -0400

    Starting to separate the normal story from personal story

--- personStory.js
+++ personStory.js
@@ -4,12 +4,7 @@ const stories = [
 
         When I was sixteen, after  particularly hard year, Mom disappeared. The townsfolk whispered, some expressed sympathy, others merely gossiped, but no one knew where she had gone. Days turned into weeks. I'd walk the town's length, looking for her, visiting her favorite spots, praying she'd just appear one day.
 
-             particular letter, dated close to her disappearance, read: "Remember,  love for you  endless, boundless.   every sunrise we've watched together, every quiet night we've shared. If ever I'm not around, find me in these memories."
-
- 
-        
-        
-              
+             particular letter, dated close to her disappearance, read: "Remember,  love for you  endless, boundless. Every sunrise we've watched together, every quiet night we've shared. If ever I'm not around, find me in these memories."   
         "`,
   },
   {
@@ -315,102 +310,7 @@ const stories = [
   {
     story: `My grandfather's old watch sits on my desk, its hands no longer moving. It was given to me on the day he passed away, a reminder of the preciousness of time. Every tick it once made echoed his heartbeats, his wisdom, and the moments we shared. Though it's silent now, its presence is a constant reminder to cherish every second and make memories that last.`,
   },
-  {
-    story: `The empty crib in the corner of the room is a stark reminder of joy anticipated but never realized. Dreams of lullabies and baby giggles remain unfulfilled, replaced by a silence that's deafening.`,
-  },
-  {
-    story: `The worn-out shoes by the doorstep have seen countless miles, each step a journey of hope, longing, and sometimes, despair. They've been witnesses to life's harshest paths and most beautiful destinations.`,
-  },
-  {
-    story: `The old tree outside the window has seen generations grow. It bears the scars of lost love, secrets whispered under its shade, and farewells that were too hard to say.`,
-  },
-  {
-    story: `The letter, unopened, lies on the table. Its seal unbroken, holding words from a time that seems like another life. A message from the past, waiting for courage to be read.`,
-  },
-  {
-    story: `The abandoned piano in the hallway, with its keys untouched, holds melodies of bygone days. Every note it once played echoed love, joy, and at times, the blues of melancholy.`,
-  },
-  {
-    story: `The dim lantern on the jetty, though flickering, still tries to light up the path. A beacon of hope for sailors long gone, and a testament to a wait that never ends.`,
-  },
-  {
-    story: `The dried inkwell on the writer's desk speaks of tales left unfinished. Stories that began with passion but were halted by life's unpredictable turns, leaving characters forever in limbo.`,
-  },
-  {
-    story: `The snow globe on the mantle, when shaken, brings a winter wonderland to life. A reminder of a holiday spent in love's warm embrace, now just a frozen memory in time.`,
-  },
-  {
-    story: `The old chapel on the hill, with its bells silent, has seen countless vows. Promises of forever, some kept till the end, while others broken by destiny's cruel jest.`,
-  },
-  {
-    story: `The solitary candle, burning in the window, is a beacon for wandering souls. Its flame, though small, is a symbol of hope, love, and memories that never fade.`,
-  },
-  {
-    story: `The dusty vinyl record spins a tune from years gone by. A dance of youth, love's first blush, and a time when every note promised eternal romance.`,
-  },
-  {
-    story: `The locket, holding a black and white photograph, is more than just a piece of jewelry. It's a portal to a time of innocence, dreams, and a love that time couldn't tarnish.`,
-  },
-  {
-    story: `The broken swing in the garden has tales to tell. Of sunlit days, childish giggles, dreams shared, and a time when the sky seemed within reach.`,
-  },
-  {
-    story: `The handwritten recipe card, stained with age and use, is more than just instructions. It's a legacy of family gatherings, shared meals, love, laughter, and moments that made life worthwhile.`,
-  },
-  {
-    story: `The withered rose pressed between the pages of a book holds more than its fragrance. It's a memento of a day when love was confessed, dreams were shared, and futures were planned.`,
-  },
-  {
-    story: `The empty bench in the park, overlooking the pond, has seen countless sunsets. Moments of solitude, reflection, promises made, and sometimes, goodbyes that left a void.`,
-  },
-  {
-    story: `The empty birdhouse in the garden, once buzzing with chirps, now stands silent. It holds memories of fledglings taking their first flight and a time when life echoed in every corner.`,
-  },
-  {
-    story: `The untouched paintbrushes, stiff with dried paint, tell of an artist's dreams left on the canvas of life. Every stroke and hue had a tale, now buried under the weight of time.`,
-  },
-  {
-    story: `The rusted keys on the ring open doors to places long forgotten. Each lock they fit into holds tales of laughter, tears, and moments that shaped lives.`,
-  },
-  {
-    story: `The silent phone, its screen cracked, holds messages of a time when words flowed freely. Conversations that once bridged distances are now memories frozen in digital ink.`,
-  },
-  {
-    story: `The worn-out journal, its pages yellowed, is a testament to battles fought in silence. Words scribbled in haste, emotions poured out, a solace found in whispered confessions to oneself.`,
-  },
-  {
-    story: `The dusty ballet slippers, pushed to the back of the closet, speak of grace, passion, and a dance with dreams. They echo pirouettes of joy and the pain of toes pressed hard against reality.`,
-  },
-  {
-    story: `The old fishing rod, its reel tangled, tells of quiet mornings by the river. Waiting for a catch, reflecting on life's ebbs and flows, and cherishing the calm before life's storms.`,
-  },
-  {
-    story: `The moth-eaten curtains, once vibrant, have witnessed years go by. They've hidden secrets, muffled cries, and let in the first rays of hope with every dawn.`,
-  },
-  {
-    story: `The abandoned farmhouse at the end of the lane stands as a relic of days gone by. Its walls have seen families grow, love stories unfold, and goodbyes that left rooms echoing with silence.`,
-  },
-  {
-    story: `The cracked teacup, though chipped, still bears the warmth of hands that once held it. Morning rituals, whispered dreams, and the solace of a brew shared in silent companionship.`,
-  },
-  {
-    story: `The faded postcard from a distant land speaks of adventures once sought. Journeys taken, horizons expanded, and the bittersweet pain of a nomad's heart finding home in transient moments.`,
-  },
-  {
-    story: `The silent grandfather clock, its pendulum still, marks an era bygone. Tick-tocks that synchronized with heartbeats, alarms that marked beginnings, and a timekeeper that witnessed life unfold.`,
-  },
-  {
-    story: `The dried-up fountain pen, its nib tarnished, wrote tales of triumph and despair. Each word it penned was a testament to feelings felt deeply and moments etched in ink.`,
-  },
-  {
-    story: `The deserted gazebo in the woods stands as a monument to secrets shared. Shadows of lovers, whispers of the night, and promises made under the canopy of stars.`,
-  },
-  {
-    story: `The stained glass window, though broken in places, paints pictures of faith held strong. Rays that colored stories of hope, love, and the eternal quest for light amidst darkness.`,
-  },
-  {
-    story: `The wind chimes, now silent, once sang songs of passing breezes. Melodies of change, tunes of transition, and a dance of elements in life's vast symphony.`,
-  },
+
   {
     story:
       "After years in a wheelchair, with endless therapy and sheer determination, I took my first step again at my daughter's wedding, dancing with her.",
@@ -423,6 +323,8 @@ const stories = [
     story:
       "Facing a life-threatening disease, every chemo session was a battle. Today, I wear my bald head with pride as a symbol of my resilience.",
   },
+
+  // joyful
   {
     story:
       "The day I held my first published book in my hands, the weight felt like holding my life's purpose.",
